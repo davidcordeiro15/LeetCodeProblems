37e716071e9a6a841aecbd6724b223a014a273e9
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-11-01 21:08:23 -0300

    Update Roman-to-Integer.java

--- Roman-to-Integer.java
+++ Roman-to-Integer.java
@@ -1,55 +1,48 @@
 class Solution {
     public int romanToInt(String s) {
-        char num; 
-        char num2; 
-        int sum = 0; // Inicializa sum
-        for (int i = 0; i < s.length(); i++) {
-            num = s.charAt(i);
-            if (i + 1 < s.length()) { // Verifica se há um próximo caractere
-                num2 = s.charAt(i + 1);
-                if (num == 'I' && num2 == 'V') {
-                    sum += 4;
-                    i++; // Pula o próximo caractere
-                    continue;
-                }
-                if (num == 'I' && num2 == 'X') {
-                    sum += 9;
-                    i++;
-                    continue;
-                }
-                if (num == 'X' && num2 == 'L') {
-                    sum += 40;
-                    i++;
-                    continue;
-                }
-                if (num == 'C' && num2 == 'M') {
-                    sum += 900;
-                    i++;
-                    continue;
-                }
-            }
-            // Adiciona o valor padrão para num
+        int sum = 0; // Inicializa a soma
+        int prevValue = 0; // Valor do caractere anterior
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char num = s.charAt(i);
+            int currentValue = 0;
+
+            // Define o valor do caractere atual
             switch (num) {
                 case 'I':
-                    sum += 1;
+                    currentValue = 1;
                     break;
                 case 'V':
-                    sum += 5;
+                    currentValue = 5;
                     break;
                 case 'X':
-                    sum += 10;
+                    currentValue = 10;
                     break;
                 case 'L':
-                    sum += 50;
+                    currentValue = 50;
                     break;
                 case 'C':
-                    sum += 100;
+                    currentValue = 100;
+                    break;
+                case 'D':
+                    currentValue = 500;
                     break;
                 case 'M':
-                    sum += 1000;
+                    currentValue = 1000;
                     break;
             }
+
+            // Se o valor atual for menor que o valor anterior, subtrai
+            if (currentValue < prevValue) {
+                sum -= currentValue;
+            } else {
+                sum += currentValue;
+            }
+
+            // Atualiza o valor anterior
+            prevValue = currentValue;
         }
+
         return sum; 
     }
 }
