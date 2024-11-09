b8d2a30f0cbe53dc7a37edee6ba198a20f972462
David_Cordeiro <160193115+davidcordeiro15@users.noreply.github.com>
2024-11-09 13:43:13 -0300

    Update MergeTwoSortedList.java

--- MergeTwoSortedList.java
+++ MergeTwoSortedList.java
@@ -25,3 +25,13 @@ class Solution {
 
     }
 }
+/*
+LinkedList
+-> She has the same ArrayList methods 
+-> The list is storage by containers
+-> Which container is linked in the first container and the next
+-> The "next" term in a container represent the next node, not the number node, but the "head"
+-> To insert a new node we need to set the head of last node and a new data
+  -> The params to insert a new node:
+      -> The last node or the last list,
+      -> A data to storage */
